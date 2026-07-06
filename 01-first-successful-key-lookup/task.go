352b167c110c5661eb5a9e9e1459cc021f4bf8e9
Siwakorn Sriveerakul <jeensiwakorn001@gmail.com>
2026-07-06 18:28:16 +0700

    finish task 01

--- 01-first-successful-key-lookup/task.go
+++ 01-first-successful-key-lookup/task.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
+	"sync"
 )
 
 type Getter interface {
@@ -12,5 +14,29 @@ type Getter interface {
 // Returns the first successful response.
 // If all requests fail, returns an error.
 func Get(ctx context.Context, getter Getter, addresses []string, key string) (string, error) {
-	return "", nil
+	if len(addresses) == 0 {
+		return "", nil
+	}
+	result := make(chan string, 1)
+	var wg sync.WaitGroup
+	for _, address := range addresses {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			val, err := getter.Get(ctx, address, key)
+			if err == nil {
+				select { // check if the channel can still send value, if so send it
+				case result <- val:
+				default: // if the channel is full, do nothing and exit the goroutine
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	select {
+	case res := <-result:
+		return res, nil
+	default:
+		return "", errors.New("All requests failed")
+	}
 }
